57c8acf145eb46be4b3d058dd7e744dcbb633393
jordanbachmann <jordan.bachmann@gmail.com>
2020-07-31 10:12:04 -0400

    cleanup and fix

--- update.mjs
+++ update.mjs
@@ -3,25 +3,18 @@ import Parser from "rss-parser";
 
 const template = (location) =>
   `<html><head><meta http-equiv="REFRESH" content="0;url=${location}"/></head><body></body></html>`;
-// `<html><head><script>window.location='${location}'</script></head><body></body></html>`;
 
 const FEED_URL =
   "https://us8.campaign-archive.com/feed?u=27254aa398528ab9c6d2ae7df&id=ce7a5d8e0f";
 
 const FILE_NAME = "index.html";
 
-const existing = fs.readFileSync(FILE_NAME, "utf8");
+const current = fs.readFileSync(FILE_NAME, "utf8");
 
 new Parser()
   .parseURL(FEED_URL)
   .then((feed) => feed.items[0])
-  .then((item) => {
-    const html = template(item.link);
-    if (existing === html) {
-      console.log("No update necessary.");
-      return Promise.resolve();
-    } else {
-      console.log(`Updating ${FILE_NAME}.`);
-      return fs.writeFileSync(FILE_NAME, html);
-    }
-  });
+  .then((item) => template(item.link))
+  .then((latest) =>
+    current !== latest ? fs.writeFileSync(FILE_NAME, latest) : Promise.resolve()
+  );
